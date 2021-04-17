279a843572b06c2bea9e4c2006c1faebb3e1d7a4
BrowningL <71730323+BrowningL@users.noreply.github.com>
2021-04-17 17:15:27 +0100

    Update imageToPDF.py

--- imageToPDF.py
+++ imageToPDF.py
@@ -4,35 +4,22 @@ import os.path
 import glob
 from natsort import natsorted
 
+#Create first variable to be turned into PDF
 im1 = Image.open(r'C:\Users\Lukem\Documents\AutomaticTextbook\PageNumber1.png')
-#image2 = Image.open(r'C:\Users\Lukem\Documents\screenshotDocuments\PageNumber2.png')
-#image3 = Image.open(r'C:\Users\Lukem\Documents\screenshotDocuments\PageNumber3.png')
-#image4 = Image.open(r'C:\Users\Lukem\Documents\screenshotDocuments\PageNumber4.png')
-
-
-#im1 = image1.convert('RGB')
-#im2 = image2.convert('RGB')
-#im3 = image3.convert('RGB')
-#im4 = image4.convert('RGB')
 
 imagelist = []
 
-
-
-#Function to create a list of all current database files created in order to compare them against username
+#Function to create a list of all current png files stored in python directory
 def databaseList():
     fileList = glob.glob((os.getcwd()) + "/*.png") # Include slash or it will search in the wrong directory!!
-    fileList = natsorted(fileList)
+    fileList = natsorted(fileList)  #Sorts file paths
     for file in fileList:
-        #print(file)
         fileNew = Image.open(file)
-        #image = fileNew.convert('RBG')
         imagelist.append(fileNew)
     return()
 
 databaseList()
-
-print(imagelist)
-
+#Gets rid of first page in list to make up for im1 variable
 imagelist.pop(0)
+#This will bring it all together into a PDF file
 im1.save(r'C:\Users\Lukem\Documents\AutomaticTextbook\myImages.pdf', save_all=True, append_images=imagelist)
